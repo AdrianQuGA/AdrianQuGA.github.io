981db545e2f650b60c932908bb6a981fe228f69b
AdrianQuGA <82419746+AdrianQuGA@users.noreply.github.com>
2021-04-13 01:21:07 -0500

    Update mi-nav.js

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -7,8 +7,7 @@ import {
 import {
   muestraError
 } from "../lib/util.js";
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-   
+
  
 class MiNav extends HTMLElement {
   connectedCallback() {
@@ -60,5 +59,3 @@ class MiNav extends HTMLElement {
 
 customElements.define(
   "mi-nav", MiNav);
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
